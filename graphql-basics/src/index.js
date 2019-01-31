132245b55ec7ad29f44b9f03216ffe1617f1e0c1
donald-stolz <donaldtstolz@gmail.com>
2019-01-31 10:48:52 -0800

    Finish lesson 10

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,4 @@
+import myAdd, { subtract } from "./math";
+
+console.log(myAdd(2, 2));
+console.log(subtract(4, 2));
---
+++ graphql-basics/src/math.js
@@ -0,0 +1,9 @@
+const add = (x, y) => {
+  return x + y;
+};
+
+const subtract = (x, y) => {
+  return x - y;
+};
+
+export { subtract, add as default };
