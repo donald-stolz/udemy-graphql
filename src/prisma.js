088f747c5e826bdf92d18665441b68ce9ee281f3
Donald Stolz <donaldtstolz@gmail.com>
2019-02-07 11:36:46 -0800

    Extract prisma secret to env

--- src/prisma.js
+++ src/prisma.js
@@ -4,8 +4,10 @@ import { fragmentReplacements } from "./resolvers";
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: "abc123",
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 });
 
 export default prisma;
+
+// abc123
